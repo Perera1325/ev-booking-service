6b8a611974b1f449c2f9d7a3f6a45778a44308d8
Perera1325 <bamunuge2002@gmail.com>
2026-02-17 11:30:19 +0530

    Day 5: Integrated WSO2 API Manager Gateway with Booking Service

--- src/main/java/com/ev/booking_service/service/BookingService.java
+++ src/main/java/com/ev/booking_service/service/BookingService.java
@@ -3,6 +3,7 @@ package com.ev.booking_service.service;
 import com.ev.booking_service.entity.Booking;
 import com.ev.booking_service.repository.BookingRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -10,12 +11,29 @@ import java.util.List;
 public class BookingService {
 
     private final BookingRepository repository;
+    private final RestTemplate restTemplate;
 
-    public BookingService(BookingRepository repository) {
+    public BookingService(BookingRepository repository, RestTemplate restTemplate) {
         this.repository = repository;
+        this.restTemplate = restTemplate;
     }
 
     public Booking saveBooking(Booking booking) {
+
+        // Call Station Service
+        String stationUrl = "http://localhost:8082/stations/" + booking.getStationId();
+
+        try {
+            Object station = restTemplate.getForObject(stationUrl, Object.class);
+
+            if (station == null) {
+                throw new RuntimeException("Station not found");
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException("Station validation failed");
+        }
+
         return repository.save(booking);
     }
 
